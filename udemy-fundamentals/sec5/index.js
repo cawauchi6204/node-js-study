d83012510e92543b1863d1f31e32224d966dc9e8
cawauchi6204 <cawauchi6204@gmail.com>
2021-08-28 14:27:57 +0900

    sec5 28完了

--- udemy-fundamentals/sec5/index.js
+++ udemy-fundamentals/sec5/index.js
@@ -1,4 +1,6 @@
-console.log('hello')
-console.trace('world')
-
-console.log('Message: %o',{hello: "world"})
+let sum = 0
+console.time("timer1")
+for (let i = 1;i <= 100000; i++) {
+  sum += i;
+}
+console.timeEnd("timer1")
