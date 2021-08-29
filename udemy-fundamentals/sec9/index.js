02f129d3e3e55e6f2b51942b5c0f925998d1855d
cawauchi6204 <cawauchi6204@gmail.com>
2021-08-29 20:54:25 +0900

    sec9 52完了

---
+++ udemy-fundamentals/sec9/index.js
@@ -0,0 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
+const data = fs.readFileSync(path.join(__dirname, 'sample.txt'), 'utf-8')
+console.log(data)
+fs.writeFileSync(path.join(__dirname, 'sample.txt'), data, 'utf-8')
