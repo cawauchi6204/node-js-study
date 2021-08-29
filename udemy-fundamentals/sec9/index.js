81fc9bb199cc6cc57a22430f67e300004b0ad3fb
cawauchi6204 <cawauchi6204@gmail.com>
2021-08-29 21:16:15 +0900

    sec9 53完了

--- udemy-fundamentals/sec9/index.js
+++ udemy-fundamentals/sec9/index.js
@@ -1,6 +1,22 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
 
-const data = fs.readFileSync(path.join(__dirname, 'sample.txt'), 'utf-8')
-console.log(data)
-fs.writeFileSync(path.join(__dirname, 'sample.txt'), data, 'utf-8')
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+  // readFile(path.join(__dirname, 'sample.txt'), 'utf-8').then((data) => {
+  //   return writeFile(path.join(__dirname, 'sample-copy.txt'), data, 'utf-8')
+  //     .then(() => { console.log('Ok') })
+  // }).catch((err) => {
+  //   console.log('indexの12行目のerrは' + err)
+  // })
+
+  (async function () {
+    try {
+      const data = await readFile(path.join(__dirname, 'sample.txt'), 'utf-8')
+      await writeFile(path.join(__dirname, 'sample-copy.txt'), data, 'utf-8')
+      console.log('ok')
+    } catch (err) {
+      console.log(err.message)
+    }
+  })();
