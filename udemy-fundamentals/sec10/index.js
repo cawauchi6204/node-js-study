7ebc1cce93d2b8fdf007c2da8fa309b0cb695033
cawauchi6204 <cawauchi6204@gmail.com>
2021-08-31 20:00:32 +0900

    sec10 64完了

--- udemy-fundamentals/sec10/index.js
+++ udemy-fundamentals/sec10/index.js
@@ -1,4 +1,5 @@
-const { URL } = require('url')
-const url = new URL('https://www.google.com/')
-console.log('indexの3行目のurl.protocolは' + url.protocol)
-console.log('indexの4行目のurl.hostは' + url.host)
+const http = require('http')
+const server = http.createServer((req, res) => {
+  res.end('Hello World')
+})
+server.listen(3000)
