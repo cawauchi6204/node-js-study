34cfe8ab3eca3d5f8789d94763f5cfcd4367c075
cawauchi6204 <cawauchi6204@gmail.com>
2021-08-31 20:07:20 +0900

    SEC10 65完了

--- udemy-fundamentals/sec10/index.js
+++ udemy-fundamentals/sec10/index.js
@@ -1,5 +1,11 @@
 const http = require('http')
+
 const server = http.createServer((req, res) => {
+  console.log(`[${(new Date()).toISOString()}]` +
+    `${req.method} ${req.url}` +
+    `${req.headers['user-agent']}`
+  )
   res.end('Hello World')
 })
+
 server.listen(3000)
