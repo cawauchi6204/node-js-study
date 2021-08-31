73b6721492f3d1f09af448849b14ac749d76840e
cawauchi6204 <cawauchi6204@gmail.com>
2021-08-31 20:17:16 +0900

    sec10 66完了

--- udemy-fundamentals/sec10/index.js
+++ udemy-fundamentals/sec10/index.js
@@ -1,11 +1,18 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer((req, res) => {
   console.log(`[${(new Date()).toISOString()}]` +
     `${req.method} ${req.url}` +
     `${req.headers['user-agent']}`
   )
-  res.end('Hello World')
+  if (req.method === 'POST') {
+    req.pipe(res)
+  } else {
+    const reader = fs.createReadStream(path.join(__dirname, 'sample.txt'), 'utf8')
+    reader.pipe(res)
+  }
 })
 
 server.listen(3000)
